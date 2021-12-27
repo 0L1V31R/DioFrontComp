8b54d26889b71771c97814bb4605a661821fe4a6
0L1V31R <eng.samuel@ymail.com>
2021-12-27 20:19:16 -0300

    added component Repository Item

--- src/components/repositories/index.js
+++ src/components/repositories/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as S from "./styled";
+import RepositoryItem from "../repository-item";
 
 const Repositories = () => {
     return (
@@ -9,8 +10,20 @@ const Repositories = () => {
                 <S.WrapperTab>Repositories</S.WrapperTab>
                 <S.WrapperTab>Starred</S.WrapperTab>
             </S.WrapperTabList>
-            <S.WrapperTabPanel>panel Repositories</S.WrapperTabPanel>
-            <S.WrapperTabPanel>panel Starred</S.WrapperTabPanel>
+            <S.WrapperTabPanel>
+                <RepositoryItem 
+                    name = "api-person-so"
+                    linktoRepo = "https://github.com/0L1V31R/api-person-so"
+                    fullName = "0L1V31R/api-person-so"
+                />
+            </S.WrapperTabPanel>
+            <S.WrapperTabPanel>
+                <RepositoryItem
+                    name= "cloneflix"
+                    linkToRepo= "https://github.com/0L1V31R/cloneflix"
+                    fullName= "0L1V31R/cloneflix"
+                />
+            </S.WrapperTabPanel>
         </S.WrapperTabs>
     );
 }
