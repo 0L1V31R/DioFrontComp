aca5bb33e8defe28a264cb5afb6f6264749fa80d
0L1V31R <eng.samuel@ymail.com>
2022-01-01 13:10:51 -0300

    refactoring repositories and starred

---
+++ src/components/repository-item/styled.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    align-content: center;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    margin: 16px 16px;
+    padding: 8px;
+    width: 20vw;
+    height: 25vh;
+`;
+
+export const WrapperTitle = styled.h2`
+    font-size: 1.2em;
+    font-weight: bold;
+    margin: 8px 0;
+`;
+
+export const WrapperFullName = styled.h4`
+    font-size: 1.1em;
+    font-weight: bold;
+    margin: 8px 0;
+    color: #2d3748;
+`;
+
+export const WrapperLink = styled.a`
+    font-size: 1em;
+    font-weight: bold;
+    margin: 8px 0;
+    color: #3182ce;
+`;
--- src/provider/github-provider.js
+++ src/provider/github-provider.js
@@ -13,6 +13,7 @@ const GithubProvider = ({ children }) => {
     const [githubState, setGithubState] = useState({
         loading: false,
         user:{
+            id: undefined,
             avatar: undefined,
             login: undefined,
             name: undefined,
@@ -40,6 +41,7 @@ const GithubProvider = ({ children }) => {
                 ...prevState,
                 hasUser: true,
                 user: {
+                    id: data.id,
                     avatar: data.avatar_url,
                     login: data.login,
                     name: data.name,
@@ -61,9 +63,31 @@ const GithubProvider = ({ children }) => {
         })
     };
 
+    const getUserRepos = ( username ) => {
+        api.get(`users/${username}/repos`).then(({ data }) => {
+            console.log("data: " + JSON.stringify(data));
+            setGithubState((prevState) => ({
+                ...prevState,
+                repositories: data,
+            }));
+        })
+    };
+
+    const getUserStarred = ( username ) => {
+        api.get(`users/${username}/starred`).then(({ data }) => {
+            console.log("data: " + JSON.stringify(data));
+            setGithubState((prevState) => ({
+                ...prevState,
+                starred: data,
+            }));
+        })
+    };
+
     const contextValue = {
         githubState,
         getUser: useCallback((username) => getUser(username), []),
+        getUserRepos: useCallback((username) => getUserRepos(username), []),
+        getUserStarred: useCallback((username) => getUserStarred(username), []),
     }
 
     return (
