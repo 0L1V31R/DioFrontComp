a4ce41a5f9875a22106e690a731a32132ecc85d5
0L1V31R <eng.samuel@ymail.com>
2022-01-01 09:45:15 -0300

    refactored initial page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Layout from "./components/layout";
 import Profile from "./components/profile";
 import Repositories from "./components/repositories";
 import useGithub from './hooks/github-hooks';
+import NoSearch from './components/no-search';
 
 const App = () => {
   const { githubState } = useGithub();
   return (
     <Layout>
-      {githubState.hasUser ?
+      {githubState.hasUser ? (
         <>
           {githubState.loading ? (
             <p>Loading</p>
@@ -19,8 +20,10 @@ const App = () => {
               <Repositories />
             </>
           )}
-        </> : <div>Nenhum usuário pesquisado</div>
-      }
+        </>
+        ) : (
+        <NoSearch />
+        )}
     </Layout>
   );
 }
